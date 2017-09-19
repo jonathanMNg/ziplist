6c92d8ead1f857b75bb7a448e947dfcf4b832d4d
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-09-19 10:45:35 -1000

    enhanced ziplist
    ziplist function now can works even if the two arrays have different length

--- ziplist.js
+++ ziplist.js
@@ -2,22 +2,19 @@ const alphabet = ['a', 'b', 'c'];
 const numbers = [1, 2, 3];
 
 function ziplist(list1, list2) {
-  if (list1.length !== list2.length) {
-    console.log("Error: Two lists don't have equal length.");
-    return null;
-  }
   const zipList = [];
-  for (let i = 0; i < list1.length; i++) {
-    zipList.push(list1[i]);
-    zipList.push(list2[i]);
+  const maxLength = Math.max(list1.length, list2.length);
+  for (let i = 0; i < maxLength; i++) {
+    if (list1[i] != null) {
+      zipList.push(list1[i]);
+    }
+    if (list2[i] != null) {
+      zipList.push(list2[i]);
+    }
   }
   return zipList;
 }
 function ziplistTheSimPleWay(list1, list2) {
-  if (list1.length !== list2.length) {
-    console.log("Error: Two lists don't have equal length.");
-    return null;
-  }
   return _.flatten(_.zip(list1, list2));
 }
 console.log(ziplist(alphabet, numbers));
