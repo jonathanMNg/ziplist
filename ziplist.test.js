7803ba2a59d09bca50f154d2715d529eca364249
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-11-21 11:28:06 -1000

    get testing to work
    using deep.equal()

--- ziplist.test.js
+++ ziplist.test.js
@@ -5,13 +5,13 @@ describe('ZipList', function () {
   const alphabet = ['a', 'b', 'c'];
   const numbers = [1, 2, 3];
   describe('ziplist()', function () {
-    it('should return 10 for ziplist', function () {
-      chai.expect(ziplist(alphabet,numbers)).to.equal(['a', 1, 'b', 2, 'c', 3]);
+    it('should return [\'a\', 1, \'b\', 2, \'c\', 3] for ziplist', function () {
+      chai.expect(ziplist(alphabet, numbers)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
     });
   });
   describe('ziplistTheSimPleWay()', function () {
-    it('should return 10 for ziplist', function () {
-      chai.expect(ziplistTheSimPleWay(alphabet,numbers)).to.equal(['a', 1, 'b', 2, 'c', 3]);
+    it('should return [\'a\', 1, \'b\', 2, \'c\', 3] for ziplistTheSimPleWay', function () {
+      chai.expect(ziplistTheSimPleWay(alphabet, numbers)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
     });
   });
 });
