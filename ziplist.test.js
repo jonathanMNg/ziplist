65df0a975de83578822d1a256bafd09fb7617f56
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-11-21 11:37:39 -1000

    check if it's a single array with 6 elements

--- ziplist.test.js
+++ ziplist.test.js
@@ -5,12 +5,20 @@ describe('ZipList', function () {
   const alphabet = ['a', 'b', 'c'];
   const numbers = [1, 2, 3];
   describe('ziplist()', function () {
-    it('should return [\'a\', 1, \'b\', 2, \'c\', 3] for ziplist', function () {
+    it('should return a single array with six elements', function () {
+      chai.expect(ziplist(alphabet, numbers)).to.be.an('array');
+      chai.expect(ziplist(alphabet, numbers)).to.have.lengthOf(6);
+    });
+    it('should deep equal the passed six elements array', function () {
       chai.expect(ziplist(alphabet, numbers)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
     });
   });
   describe('ziplistTheSimPleWay()', function () {
-    it('should return [\'a\', 1, \'b\', 2, \'c\', 3] for ziplistTheSimPleWay', function () {
+    it('should return a single array with six elements', function () {
+      chai.expect(ziplistTheSimPleWay(alphabet, numbers)).to.be.an('array');
+      chai.expect(ziplistTheSimPleWay(alphabet, numbers)).to.have.lengthOf(6);
+    });
+    it('should deep equal the passed six elements array', function () {
       chai.expect(ziplistTheSimPleWay(alphabet, numbers)).to.deep.equal(['a', 1, 'b', 2, 'c', 3]);
     });
   });
